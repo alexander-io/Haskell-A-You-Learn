e71eb65110bd7ea63c3ca2b909dcdc81810e38f3
alexander-io <abharris@pugetsound.edu>
2018-09-04 18:49:02 -0700

    ++ chapter 02 list comprehensions

--- chapters/02/chap_02_expressions.hs
+++ chapters/02/chap_02_expressions.hs
@@ -62,6 +62,8 @@ calcSum list = sum list
 -- product, takes a list of numbers and returns their product
 calcProduct list = product list
 
+---- List Ranges / Texas Ranges ----
+
 -- using '..' a range of sequential values can be list-ified
 makeListRange x xx = [x..xx]
 -- NOTE to make a  list from  20 -> 1,  you can't just issue [20..1] ...
@@ -81,3 +83,37 @@ repeatAndTake rep n = take n (repeat rep)
 
 -- replicate, takes an element and produces an infinite  list of  just  that element. like  cycling a list with only one element
 replicateAndTake rep n = take n (replicate n rep)
+
+---- List Comprehension ----
+-- mathematics has a  similar concept called set comprehensions
+-- normally used  for  building more specific sets
+
+-- list comprehension : for every x in range 1 to 10
+-- multiply x by 2
+-- ghci> [x*2 | x <- [1..10]]
+
+-- for every x in range 1 to 10
+-- multiply  x by 2
+-- include in result if x*2 >= 12 // true
+-- ghci> [x*2 | x <- [1..10], x*2 >= 12]
+
+-- all values from 50 to 100 whose remainder when divided with the number 7 is 3
+-- ghci> [ x | x <- [50..100], x `mod` 7 == 3 ]
+
+-- replace each odd  number greater than 1 with bang and each odd number less  than 10 with  boom
+-- if a number isn't odd, throw away
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+-- boomBangs [7..13]
+-- ["BOOM!","BOOM!","BANG!","BANG!"]
+
+-- all numbers from 10 to  20 that  are  not (exclude) 13, 15, 19
+-- ghci> [x | x <- [10..20], x /= 13, x /= 15, x /=19]
+
+-- when drawing from several  lists, comprehensions produce all combinations  of the given lists and then join them by the output function we supply
+
+-- a  list produced by a comprehension  that  draws from  two lists of length 4 will have a length of 16, provided we don't filter them
+
+-- if we have two lists  [2,5,10] and [8,10,11] and we want to  get the products of all the possible combinations between numbers in those lists, here's what we'd do
+
+-- [x*y | x <- [2,5,10], y <- [8,10,11]]
+-- [16,20,22,40,50,55,80,100,110]
